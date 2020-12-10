cca8fbcf7fdf8fa55d1bbdc8e6c9e795dbbc516f
Jingyu He <gtouchgogo@163.com>
2020-12-11 01:24:04 +0800

    bugfix

--- postgresql/docker-entrypoint-initdb.d/init-user-db.sh
+++ postgresql/docker-entrypoint-initdb.d/init-user-db.sh
@@ -3,4 +3,5 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/superUser
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/ejabberd 
-
+mkdir -p /etc/postgresql
+cp /docker-entrypoint-initdb.d/pg_hba.conf /etc/postgresql/pg_hba.conf
