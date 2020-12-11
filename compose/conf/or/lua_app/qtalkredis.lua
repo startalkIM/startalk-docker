d074cf36a5a61d71f0df30d9997ef9dcd1e0097b
Jingyu He <gtouchgogo@163.com>
2020-12-11 15:55:37 +0800

    bugfix

---
+++ compose/conf/or/lua_app/qtalkredis.lua
@@ -0,0 +1,16 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: qitmac000378
+-- Date: 17/5/5
+-- Time: 下午5:42
+-- To change this template use File | Settings | File Templates.
+--
+
+local _M = {}
+_M.redis = {}
+_M.redis.host = 'startalk-redis'
+_M.redis.port = '6379'
+_M.redis.subpool = 2
+_M.redis.passwd = '123456'
+
+return _M
